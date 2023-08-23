8c8859fd856889b54f0cf45cba526a3c317eb7a1
Alexia Skrz <alexia.sireuille@gmail.com>
2023-08-23 11:10:26 +0200

    use UseEffect in  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import ProfilPage from './components/pages/ProfilPage/ProfilPage'
 
 import './App.css'
 
-const formattedData = await formatData(12, false)
+// const formattedData = await formatData(12, false)
 
 
 export default function App ()
 {
+  const [ loading, setLoading ] = useState( false )
+  const [ formattedData, setFormattedData ] = useState( 12, false )
   
+  useEffect( () =>
+  {
+    async function initialisation ()
+    {
+      const data = await formatData( 12, false )
+      setFormattedData( data )
+      setLoading(data)
+    }
+    initialisation()
+  }, [ loading ] )
+
   return (
     <div className='App'>
-      <ProfilPage
+      { loading && (
+        <ProfilPage
         firstname={ formattedData.firstname }
         nutrition={formattedData.nutritionData}
         activities={ formattedData.activity }
@@ -20,7 +34,8 @@ export default function App ()
         labelsKind={ formattedData.labelsKind }
         userScore={ formattedData.userScore }
         progressInPercentage={ formattedData.progressInPercentage }
-      />
+        />
+      ) }
     </div>
   )
 }
